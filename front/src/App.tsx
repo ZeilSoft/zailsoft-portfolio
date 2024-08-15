efe153c13d9fe267f3bb467f264fe31cc10b744d
Nievas2 <angelgabrielnievas@gmail.com>
2024-08-15 18:06:44 -0300

    feat(Front): :sparkles: Add navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,11 @@
-function App() {
+import Navbar from "./pages/home/components/Navbar"
+import HomePage from "./pages/home/homePage"
 
+function App() {
   return (
     <main className="font-poppins">
-        <h1>App</h1>
+      <Navbar />
+      <HomePage />
     </main>
   )
 }
